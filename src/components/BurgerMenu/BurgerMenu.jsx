8f552fe919d7d52b24f0052ae14b6df49db1984a
infinitidevs <andreshiguita9@gmail.com>
2023-09-14 23:50:08 +0200

    feat: add header and hero components

---
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Btn, BtnWrapper, Toggle } from './styles';
+
+const BurgerMenu = () => {
+  return (
+    <>
+      <Toggle />
+      <BtnWrapper>
+        <Btn></Btn>
+      </BtnWrapper>
+    </>
+  );
+};
+
+export default BurgerMenu;
